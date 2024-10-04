c4fc043d5bdc0d6eb021ed2a86a1a7c490e9974c
Floris de Bijl <floris.debijl@gmail.com>
2024-10-04 14:36:44 +0200

    Cleanup config

--- README.md
+++ README.md
@@ -8,7 +8,17 @@ Usage:
 ```sh
 yarn add -DE @feedbackfruits/eslint-config
 ```
+Flat config:
+*eslint.config.mjs*
+```js
+import feedbackfruits from '@feedbackfruits/eslint-config';
 
+export default [
+  ...feedbackfruits
+];
+```
+
+Legacy config:
 *.eslintrc.json*
 ```json
 {
@@ -16,4 +26,30 @@ yarn add -DE @feedbackfruits/eslint-config
 }
 ```
 
-Any rules or extra environments can be overriden by adding the config keys as usual
+Any rules or extra environments can be overriden by adding the config keys as usual:
+
+Flat config:
+*eslint.config.mjs*
+```js
+import feedbackfruits from '@feedbackfruits/eslint-config';
+
+export default [
+  ...feedbackfruits,
+  {
+    rules: {
+      'no-console': 'off'
+    }
+  }
+];
+```
+
+Legacy config:
+*.eslintrc.json*
+```json
+{
+  "extends": "@feedbackfruits",
+  "rules": {
+    "no-console": "off"
+  }
+}
+```
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,93 +1,78 @@
-import path from "node:path";
-import { fileURLToPath } from "node:url";
+import globals from 'globals';
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import tsParser from '@typescript-eslint/parser';
+import importPlugin from 'eslint-plugin-import';
 
-import globals from "globals";
-import js from "@eslint/js";
-import tsParser from "@typescript-eslint/parser";
-import { fixupConfigRules } from "@eslint/compat";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all
-});
-
-export default [...fixupConfigRules(compat.extends(
-  "eslint:recommended",
-  "plugin:import/recommended",
-  "plugin:import/typescript",
-  "plugin:@typescript-eslint/recommended",
-)), {
-  languageOptions: {
-    globals: {
-      ...globals.node,
-    },
-
-    parser: tsParser,
-    ecmaVersion: 2020,
-    sourceType: "module",
-
-    parserOptions: {
-      ecmaFeatures: {
-        legacyDecorators: true,
+export default tseslint.config(
+  eslint.configs.recommended,
+  importPlugin.flatConfigs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+      parser: tsParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: {
+          legacyDecorators: true,
+        },
       },
     },
-  },
-
-  settings: {
-    "import/resolver": {
-      node: {
-        extensions: [".js", ".jsx", ".ts", ".tsx", ".d.ts"],
+    settings: {
+      'import/resolver': {
+        node: {
+          extensions: ['.js', '.jsx', '.ts', '.tsx', '.d.ts'],
+        },
       },
     },
-  },
-
-  rules: {
-    "@typescript-eslint/ban-types": 0,
-    "@typescript-eslint/camelcase": 0,
-    "@typescript-eslint/explicit-module-boundary-types": 0,
-    "@typescript-eslint/no-explicit-any": 1,
-    "@typescript-eslint/no-inferrable-types": 1,
-    "@typescript-eslint/no-unused-vars": 1,
-    "@typescript-eslint/no-var-requires": 0,
-    "@typescript-eslint/prefer-as-const": 0,
-    "array-type": 0,
-    "arrow-parens": 0,
-    curly: 0,
-    "default-case-last": 1,
-    "import/export": 0,
-    "import/no-deprecated": 2,
-    "import/no-named-as-default": 0,
-    "import/order": 1,
-    "interface-over-type-literal": 0,
-    "max-line-length": 0,
-    "no-async-promise-executor": 1,
-    "no-await-in-loop": 0,
-    "no-confusing-arrow": 1,
-    "no-console": 0,
-    "no-eval": 2,
-    "no-implied-eval": 2,
-    "no-labels": 1,
-    "no-multiple-empty-lines": 1,
-    "no-plusplus": 0,
-    "no-return-assign": 1,
-    "no-return-await": 1,
-    "no-sequences": 1,
-    "no-shadow": 1,
-    "no-shadowed-variable": 0,
-    "no-string-literal": 0,
-    "no-trailing-spaces": 1,
-    "object-curly-newline": 0,
-    "object-literal-key-quotes": 0,
-    "object-literal-sort-keys": 0,
-    "ordered-imports": 0,
-    "prefer-const": 1,
-    quotes: [2, "single"],
-    "space-before-function-paren": 0,
-    "trailing-comma": 0,
-    yoda: 1,
-  },
-}];
+    rules: {
+      '@typescript-eslint/ban-types': 0,
+      '@typescript-eslint/camelcase': 0,
+      '@typescript-eslint/explicit-module-boundary-types': 0,
+      '@typescript-eslint/no-explicit-any': 1,
+      '@typescript-eslint/no-inferrable-types': 1,
+      '@typescript-eslint/no-unused-vars': 1,
+      '@typescript-eslint/no-var-requires': 0,
+      '@typescript-eslint/prefer-as-const': 0,
+      'array-type': 0,
+      'arrow-parens': 0,
+      'curly': 0,
+      'default-case-last': 1,
+      'import/export': 0,
+      'import/no-deprecated': 2,
+      'import/no-named-as-default': 0,
+      'import/order': 1,
+      'interface-over-type-literal': 0,
+      'max-line-length': 0,
+      'no-async-promise-executor': 1,
+      'no-await-in-loop': 0,
+      'no-confusing-arrow': 1,
+      'no-console': 0,
+      'no-eval': 2,
+      'no-implied-eval': 2,
+      'no-labels': 1,
+      'no-multiple-empty-lines': 1,
+      'no-plusplus': 0,
+      'no-return-assign': 1,
+      'no-return-await': 1,
+      'no-sequences': 1,
+      'no-shadow': 1,
+      'no-shadowed-variable': 0,
+      'no-string-literal': 0,
+      'no-trailing-spaces': 1,
+      'object-curly-newline': 0,
+      'object-literal-key-quotes': 0,
+      'object-literal-sort-keys': 0,
+      'ordered-imports': 0,
+      'prefer-const': 1,
+      'quotes': [2, 'single'],
+      'space-before-function-paren': 0,
+      'trailing-comma': 0,
+      'yoda': 1,
+    },
+  }
+);
