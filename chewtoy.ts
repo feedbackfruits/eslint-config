2052957437a5eff165f1d3e92ce0f95d3c54f74a
Floris de Bijl <floris.debijl@gmail.com>
2024-10-04 14:41:34 +0200

    Extend chewtoy

--- chewtoy.ts
+++ chewtoy.ts
@@ -1,4 +1,10 @@
 // Give eslint something to chew on to validate the config
+// Run `yarn validate`, you should get no errors or warnings, if you uncomment the lines below
+// you should get a warning about the unused variable, double import, quotes and redundant type
 
+// import fs from 'fs';
+// import fs2 from 'fs';
+
+// const bar: string = "foo";
 const foo = 'bar';
 console.log(foo);
